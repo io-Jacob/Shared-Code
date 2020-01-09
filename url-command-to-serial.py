d2f64992b7000af914bac8f3771bd3f67897323a
io-Jacob <web@controlanything.com>
2020-01-09 10:40:06 -0600

    added prints

--- url-command-to-serial.py
+++ url-command-to-serial.py
@@ -3,7 +3,7 @@ import time
 import re
 
 
-commandURI = "http://192.168.1.12/cgibin/runcommand.sh?rndvar:cmd=255,109,1r1t300:cmd=254,110,1r1t300:cmd=254,101,1r1t300"
+commandURI = "http://192.168.1.12/cgibin/runcommand.sh?rndvar:cmd=254,109,1r1t300:cmd=254,110,1r1t300:cmd=254,101,1r1t300"
 
 # split the string using colon (:) as the delimiter and then remove the first two items.
 commandList = commandURI.split(':')[2:]
@@ -15,12 +15,15 @@ returnData = ""
 for command in commandList:
     # regex down to the command bytes to send, split into array by commas
     commandData = re.search("cmd=(.*)r", command).group(1).split(',')
+    print("Line 18: " + str(commandData))
 
     # regex down to the number of bytes to expect back, convert into integer
     returnBytes = int(re.search("r(.*)t", command).group(1))
+    print("Line 22: " + str(returnBytes))
 
     #regex down to the timeout on the command, convert into integer
     timeout = int(re.search("t(.*)$", command).group(1))
+    print("Line 26: " + str(timeout))
 
     # convert array from string to int
     for i in range(0, len(commandData)):
@@ -35,9 +38,10 @@ for command in commandList:
 
     # run loop to read serial port until the number of expected back are returned or timeout is reached
     while startTime > (time.time()*1000-timeout) and len(readBuffer) != returnBytes:
-        read = serialPort.read(returnBytes)
-        if read:
-            readBuffer.append(read)
+        readBuffer.append(serialPort.read(returnBytes))
+        # read = serialPort.read(returnBytes)
+        # if len(read):
+        #     readBuffer.append(read)
 
     # return complete at beginning if command succeeded, return e_serial_timeout if failed
     if len(readBuffer) == returnBytes:
@@ -52,7 +56,7 @@ for command in commandList:
     # add it to data to be returned, remove trailing comma (,)
     returnData += byteString[:-1]+"\n"
 
-print(returnData)
+print("Line 59 - Response Data: \n"+returnData)
 serialPort.close()
 # this is strictly to pause the script after reading
 input('')
